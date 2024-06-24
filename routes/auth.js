adfec98cd3b4b8a7fa44fa07a7d4f54d1b62c415
Taufan Ali <126685412+taufanAli65@users.noreply.github.com>
2024-06-24 10:07:13 +0700

    fix admin login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Pelanggan} = require('../models');
+const {Pelanggan, PemilikToko} = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -15,20 +15,44 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
+router.post('/registerAdmin', async(req, res, next) => {
+  try {
+    const { username, password, email } = req.body;
+    const newAdmin = await PemilikToko.create({username, password, email});
+    res.status(201).json({ message: 'Admin Added Successfully'});
+  } catch(err) {
+    next(err);
+  }
+})
+
 // Rute login
 router.post('/login', async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const pelanggan = await Pelanggan.findOne({ where: { username } });
-    if (!pelanggan) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+    const pemilikToko = await PemilikToko.findOne({ where: { username } });
+
+    // Pengecekan apakah username ditemukan di antara pelanggan atau pemilikToko
+    if (!pelanggan && !pemilikToko) {
+      return res.status(401).json({ message: 'Username Salah' });
     }
-    const isMatch = await bcrypt.compare(password, pelanggan.password);
+
+    // Memeriksa kecocokan password jika username ditemukan
+    let isMatch = false;
+    if (pelanggan && pelanggan.password) {
+      isMatch = await bcrypt.compare(password, pelanggan.password);
+    } else if (pemilikToko && pemilikToko.password) {
+      isMatch = await bcrypt.compare(password, pemilikToko.password);
+    }
+
+    // Memberikan respons sesuai dengan kecocokan password
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Password Salah' });
     }
+
+    // Membuat token JWT jika username dan password cocok
     const token = jwt.sign(
-      { id: pelanggan.id},
+      { id: pelanggan ? pelanggan.id : pemilikToko.id },
       'your_jwt_secret',
       { expiresIn: '1h' }
     );
